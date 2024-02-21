14af9752cc5a88edd16eff6ce484d59f5c2e2709
KwakMinJae <575alswo75@naver.com>
2024-02-21 23:54:40 +0900

    CheckBox.tsx 작성
    CheckBox.tsx작성

---
+++ src/CheckBox.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface CheckBoxProps{
+    checked?:boolean;
+    onClick?():void;
+}
+
+const CheckBox = ({
+    checked,
+    onClick
+}: CheckBoxProps) =>{
+    return(
+        <>
+            <div className='container' onClick={onClick}>
+                <div className='checkIcon'>
+                    {checked && "0"}
+                </div>
+            </div>
+        </>
+    )
+}
